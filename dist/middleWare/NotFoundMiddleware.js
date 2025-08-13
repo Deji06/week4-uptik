d349b2e1e1c9b067bb8709e27c6b50b647e1b984
Oladeji <olawunioladeji06@gmail.com>
2025-08-13 11:40:02 +0100

    Remove dist from Git, update Note.ts to tasks, fix tsconfig

--- dist/middleWare/NotFoundMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const http_status_codes_1 = require("http-status-codes");
-const NotFoundMiddleware = (req, res) => {
-    res.status(http_status_codes_1.StatusCodes.NOT_FOUND).send('page does not exist');
-};
-exports.default = NotFoundMiddleware;
